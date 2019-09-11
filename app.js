bb22f10a8c49f021add0e94f126192518582e848
Utkarsh Mittal <umutkarsh@gmail.com>
2019-09-11 12:30:01 +0530

    basic controller and routing structure

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const feedRoutes = require('./routes/feed');
 
 const app = express();
 
-console.log('ab');
+app.use(express.json());
+
+app.use('/feed', feedRoutes);
 
 app.listen(8080);
