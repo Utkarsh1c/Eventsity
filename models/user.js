63512d818636a82f22a3ba2fbc335e48a747176c
Palak2820 <54177848+Palak2820@users.noreply.github.com>
2019-09-16 01:28:17 +0530

    user

---
+++ models/user.js
@@ -0,0 +1,22 @@
+module.export =(sequelize,type) =>{
+
+    return sequelize.define('user',{
+        id:{
+            type:type.INTEGER,
+            primaryKey:true,
+            autoIncrement:true
+        },
+        first_name:type.STRING,
+        last_name:type.STRING,
+        email:type.STRING,
+        username:{
+            type:type.STRING,
+            allowNull:false
+        },
+        password: {
+            type:type.STRING,
+            allowNull:false
+        }
+
+    })
+};
