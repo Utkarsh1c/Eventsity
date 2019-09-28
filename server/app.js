c98bb04d830f1dd86e2db877c7b2de4412ee6fad
Utkarsh Mittal <umutkarsh@gmail.com>
2019-09-28 21:51:46 +0530

    added admin panel

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ const Otp = require('./models/otp');
 const feedRoutes = require('./routes/feed');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
+const adminRoutes = require('./routes/admin');
 
 const app = express();
 
@@ -33,6 +34,7 @@ app.use((req, res, next) => {
 app.use('/feed', feedRoutes);
 app.use('/auth', authRoutes);
 app.use('/user', userRoutes);
+app.use('/admin', adminRoutes);
 
 app.use((error, req, res, next) => {
   console.log(error);
