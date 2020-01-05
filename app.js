6ca2a7073efbfa17ddcd69d444ad5dc03506f7d6
Utkarsh Mittal <umutkarsh@gmail.com>
2020-01-05 23:50:23 +0530

    bug fixes

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ sequelize
   .sync()
   .then(() => {
     
-    app.listen(process.env.PORT || 3000, function(){
+    app.listen(process.env.port || 3000, function(){
       console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
     });
 
